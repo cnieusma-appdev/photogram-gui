a5a9c0cd2b8ce550b5a6f75e1f283d16098e69ee
cnieusma <cnieusma@uchicago.edu>
2023-03-09 08:20:18 +0000

    update photos

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,17 @@
 class UsersController < ApplicationController
   def index
     matching_users = User.all
-    @list_of_users = matching_users.order({ :usernmae => :asc })
+    @list_of_users = matching_users.order({ :username => :asc })
     render({ :template => "user_templates/index.html.erb"})
   end
 
   def show
       url_username = params.fetch("path_username")
       matching_usernames = User.where({ :username => url_username })
-      the_user = matching_usernames.at(0)
+      @the_user = matching_usernames.first
 
-      if the_user == nil
-        redirect_to("/404")
-      else
-        render({ :template => "user_templates/show.html.erb"})
-      end
+      render({ :template => "user_templates/show.html.erb"})
+
   end
 
 end
