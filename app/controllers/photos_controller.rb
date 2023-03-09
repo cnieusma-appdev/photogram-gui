d070dad4a4795ca2034d839c3b9c77d16430e7b4
cnieusma <cnieusma@uchicago.edu>
2023-03-09 09:10:50 +0000

    delete photo

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -13,4 +13,14 @@ class PhotosController < ApplicationController
     render({ :template => "photo_templates/show.html.erb"})
   end
 
+  def destroy
+    the_id = params.fetch("toast_id")
+    matching_photos = Photo.where({ :id => the_id })
+    the_photo = matching_photos.at(0)
+
+    the_photo.destroy
+    
+    render({ :template => "photo_templates/destroy.html.erb"})
+  end
+
 end
